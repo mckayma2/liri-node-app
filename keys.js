6a42bd38c695d20fc2844124eefd8f037e192129
ADM MacKay <adminmama@marineharvest.net>
2018-01-13 10:27:12 -0500

    additional sudo code comments

--- keys.js
+++ keys.js
@@ -5,7 +5,9 @@ var spotifyKey = {
 clientId: '2a974e81e3694af08ad4d0fb94a021f9',
 clientSecret: '2b8265c58b2149bd9d8b58b2a629f2e6'
 }
+//to export spotify key object
 module.exports.spotifyKey = spotifyKey;
 
 
 //sudo code -- where i would place other
+//create other objects for additional api keys
